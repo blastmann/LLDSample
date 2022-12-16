15fa286e8e538dd35847473561d78f072e3b4d69
eddiecmchen <eddiecmchen@tencent.com>
2022-12-16 14:14:16 +0800

    Initial Commit

---
+++ LLDSample/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LLDSample
+//
+//  Created by eddiecmchen on 2022/12/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LLDSample/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LLDSample
+//
+//  Created by eddiecmchen on 2022/12/16.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
